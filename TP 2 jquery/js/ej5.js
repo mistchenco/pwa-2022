108c07c965ddc94a3022e63eb26fe8a291fc97e8
matiasperaltamacri <matias.peralta@est.fi.uncoma.edu.ar>
2022-05-07 19:04:45 -0300

    ejercicio 5 idea

--- TP 2 jquery/js/ej5.js
+++ TP 2 jquery/js/ej5.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $.getJSON('./php/leer_usuarios.php',function(data){
         var $cantUs= data.length; //Cantidad de usuarios. Para generar las tablas.
+        var $cantPag=$cantUs/5;
+        $cantPag=Math.ceil($cantPag); //Redondea para arriba. Cantidad de paginas a usar
         
     });
 })
